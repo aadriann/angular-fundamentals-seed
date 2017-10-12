8511443d9943c796c3330cc15c4b393285109d30
Adrian Rincon Lopez <adrianrl9441@gmail.com>
2017-10-12 18:28:14 +0200

    Template Fundamental Lessons

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,13 +2,64 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  styleUrls: ['app.component.scss'],
+  styleUrls: ['app.component.scss'], // Contain a reference of our Sass file
   template: `
     <div class="app">
-      Hello!
+      <!--<h1 [innerHTML]="title"></h1>
+      <h1>{{ title + '!'}}</h1>
+      <img [src]="logo"/>-->
+      <!--<button (click)="handleClick(username.value)">
+        Get Value
+      </button>-->
+      <!--<input 
+        type="text" 
+        [ngModel] = "name"
+        (ngModelChange)="handleChange($event)">-->
+      <!--<input type="text" #username/>-->
+
+        <!--<input 
+        type="text" 
+        [(ngModel)] = "name">-->
+      <input 
+        type="text" 
+        [value] = "name"
+        (input)="handleChange($event.target.value)">
+
+      <template [ngIf] ="name.length > 2">
+        <div>
+          Searching for... {{ name }}
+        </div>
+      </template>
+
+      <div *ngIf="name.length > 2">
+        Searching for... {{ name }}
+      </div>
+      <!--<div>
+        {{ numberOne + numberTwo }}
+      </div>
+      <div>
+        {{ isHappy ? ':)' : ':(' }}
+      </div>-->
     </div>
-  `
+    `
 })
 export class AppComponent {
+  title: string;
+  name: string = '';
+  logo: string = 'img/logo.svg';
+  numberOne: number = 1;
+  numberTwo: number = 2;
+  isHappy: boolean = true;
 
+  constructor() {
+    this.title = 'Ultimate Angular';
+  }
+
+  /*handleClick(value: string) {
+    console.log(value);
+  }*/
+  handleChange(value: string) {
+    this.name = value;
+    console.log(event);
+  }
 }
